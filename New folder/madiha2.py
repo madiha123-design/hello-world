a72f8da01849e576dc2db8d6520c8ef3520f8932
madiha123-design <70858557+madiha123-design@users.noreply.github.com>
2020-09-13 19:15:06 +0530

    Hey

---
+++ New folder/madiha2.py
@@ -0,0 +1,10 @@
+temp=float(input('enter the temp'))
+if temp<20:
+    print('cold temperature')
+elif (temp>=20 and temp<=40):
+    print('good atmosphere')
+elif (temp>=40 and temp<=60):
+    print('hot')
+else:
+    print('wrong input')
+    
